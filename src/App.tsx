d479c0e1723761b0e96906ea7a3a581e596f0517
Daniel Forson <danielkojoforson27@gmail.com>
2024-01-23 13:08:59 +0100

    refactored the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import QuestionsPage from './pages/QuestionsPage';
 import CompletedPage from './pages/completed/CompletedPage';
 
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="questions/:quizId" element={<QuestionsPage />} />
         <Route path="completion" element={<CompletedPage />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
